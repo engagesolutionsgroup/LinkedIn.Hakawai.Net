d1104f34d3129e3c0154b19388ee6147cc3951bf
Artem Valieiev <artemvalieiev@outlook.com>
2023-12-15 18:41:06 +0100

    Add support for maccatalyst

--- xcode/build.sh
+++ xcode/build.sh
@@ -3,16 +3,24 @@
 # Define the output directories
 IPHONEOS_OUTPUT_DIR="../output/Frameworks/Release-iphoneos"
 IPHONESIMULATOR_OUTPUT_DIR="../output/Frameworks/Release-iphonesimulator"
+# Define the output directory for Mac Catalyst
+MACCATALYST_OUTPUT_DIR="../output/Frameworks/Release-maccatalyst"
+
+# Run xcodebuild for Mac Catalyst (both x86_64 and arm64)
 XCFRAMEWORK_OUTPUT_DIR="../output/"
 FRAMEWORK_NAME="Hakawai"
 # Run xcodebuild for iphoneos
-xcodebuild -configuration Release -sdk iphoneos CONFIGURATION_BUILD_DIR="$IPHONEOS_OUTPUT_DIR"
+#xcodebuild archive -scheme Hakawai -destination "generic/platform=iOS" -archivePath $IPHONEOS_OUTPUT_DIR
 
 # Run xcodebuild for iphonesimulator
-xcodebuild -configuration Release -sdk iphonesimulator CONFIGURATION_BUILD_DIR="$IPHONESIMULATOR_OUTPUT_DIR"
+#xcodebuild archive -scheme Hakawai -destination "generic/platform=iOS Simulator" -archivePath $IPHONESIMULATOR_OUTPUT_DIR
+
+#Run xcodebuild for MacCatalyst
+#xcodebuild archive -scheme Hakawai -destination "generic/platform=macOS,variant=Mac Catalyst" -archivePath $MACCATALYST_OUTPUT_DIR
 
 #create xcframework 
 xcodebuild -create-xcframework \
-    -framework "$IPHONEOS_OUTPUT_DIR/$FRAMEWORK_NAME.framework" \
-    -framework "$IPHONESIMULATOR_OUTPUT_DIR/$FRAMEWORK_NAME.framework" \
-    -output "$XCFRAMEWORK_OUTPUT_DIR/$FRAMEWORK_NAME.xcframework"
+   -archive "$IPHONEOS_OUTPUT_DIR.xcarchive"  -framework $FRAMEWORK_NAME.framework \
+   -archive "$IPHONESIMULATOR_OUTPUT_DIR.xcarchive" -framework $FRAMEWORK_NAME.framework \
+   -archive "$MACCATALYST_OUTPUT_DIR.xcarchive" -framework $FRAMEWORK_NAME.framework \
+   -output "$XCFRAMEWORK_OUTPUT_DIR/$FRAMEWORK_NAME.xcframework" 
